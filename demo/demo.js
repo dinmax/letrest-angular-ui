620ff73835af0ecb3ef524490bd2c0030ac892a4
sminutoli <sminutoli.it@gmail.com>
2015-02-05 19:15:05 -0300

    add demo
    add demo for testing
    add bower to use demo
    fixes some build settings

---
+++ demo/demo.js
@@ -0,0 +1,10 @@
+/**
+ * Created by sminutoli on 05/02/15.
+ */
+
+angular.module('Letrest', ['let-rest']);
+
+angular.module('Letrest').controller('mainCtrl', function(LetRestSecurityService){
+  this.message = 'Hello Letrest';
+  debugger;
+});
